9aae86808583ac368f29f834bc6fbcc0c95cad9d
yayat <yayat.ruhyana@outlook.com>
2023-07-23 01:40:50 +0700

    first commit

---
+++ layouts/footer.php
@@ -0,0 +1,23 @@
+     </div>
+     
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
+  <script type="text/javascript" src="libs/js/functions.js"></script>
+  </body>
+  <style>
+    
+    .footer {
+      background-color: #f5f5f5;
+      padding: 20px;
+      text-align: center;
+      color: #888;
+    }
+  </style>
+<footer class="footer">
+    <p>Yayat Rhyna</p>
+  </footer>
+</html>
+
+<?php if(isset($db)) { $db->db_disconnect(); } ?>
